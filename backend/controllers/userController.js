9f63e3cffcc66ba19044ce4a788c907d32956178
Aditya Singh <adityaa9170843@gmail.com>
2025-09-15 22:45:28 +0530

    password hashing

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,34 +1,58 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 
+const generateToken = (user) => {
+  const token = jwt.sign(
+    {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    },
+    process.env.JWT_SECRET,
+    {
+      expiresIn: "30d",
+    }
+  );
+  return token;
+};
 
+export const loginUser = async (req, res) => {};
 
-export const loginUser = async(req, res) =>{
+export const registerUser = async (req, res) => {
+  const { name, email, password } = req.body;
 
-}
+  try {
+    const existUser = await UserModel.findOne({ email });
+    if (existUser) {
+      return res.status(400).json({ message: "User already exist" });
+    }
 
-
-
-
-export const registerUser = async(req, res) =>{
-    const {name, email, password} = req.body
-
-    try{
-        const existUser = await UserModel.findOne({email});
-        if(existUser){
-            return res.status(400).json({message: "User already exist"});
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        if (err) {
+          res.status(400);
+          throw new Error("Error hashing password");
         }
-    }
-    catch(error){
-        console.log(error);
-    }
-}
+        const user = await UserModel.create({
+          name,
+          email,
+          password: hash,
+        });
+        let token = generateToken(user);
+        res.cookie("token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "None",
+        });
 
-
+        res.status(201).json({ message: "User created successfully", user });
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 //admin route
 
-
-export const adminLogin = async(req, res) =>{
-
-}
+export const adminLogin = async (req, res) => {};
