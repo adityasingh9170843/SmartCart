1cf38362bfdb3dc4f2592618ffc616403f925906
Aditya Singh <adityaa9170843@gmail.com>
2025-09-16 22:47:04 +0530

    added cloudinary function to upload images and get urls back

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,20 +1,37 @@
 import ProductModel from "../models/productModel.js";
-
+import { cloudinary } from "../config/cloudinary.js";
 
 
 
 export const addProduct = async( req, res ) =>{
     try{
         const {name,description,price,image,category,subCategory,sizes} = req.body;
-        const image1 = req.files.image1[0];
-        const image2 = req.files.image2[0];
-        const image3 = req.files.image3[0];
-        const image4 = req.files.image4[0];
+        const image1 =req.files.image1 && req.files.image1[0];
+        const image2 =req.files.image2 && req.files.image2[0];
+        const image3 =req.files.image3 && req.files.image3[0];
+        const image4 =req.files.image4 && req.files.image4[0];
         
-         
+        const images = [image1,image2,image3,image4].filter((item)=> item!=undefined);
+        
+        let imagesUrl = Promise.call(images.map(async (image) => {
+            const result = await cloudinary.uploader.upload(image.path,{resource_type:"image"});
+             return result.secure_url;
+        })) 
+
+        const product = await ProductModel.create({
+            name,
+            description,
+            price,
+            image:imagesUrl,
+            category,
+            subCategory,
+            sizes
+        });
+        res.status(200).json(product);
 
     }
     catch(error){
+        res.status(400).json({message:error.message});
     }
 }
 
